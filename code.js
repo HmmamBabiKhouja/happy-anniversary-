accc63a4e63b0b2a040c7ffe16e91dfada7a258d
Hmmam Babi Khouja <bhmmam@gmail.com>
2021-10-08 18:47:56 +0200

    adding the cake

--- code.js
+++ code.js
@@ -1,12 +1,21 @@
 let body = document.querySelector("body")
-let gift = document.querySelector(".gift").addEventListener("click", openGift);
+let gift = document.querySelector(".gift");
 let giftTop = document.querySelector(".gift-top");
 let giftText = document.querySelector(".gift-text");
 let giftBox = document.querySelector(".gift-box");
+let giftContent = document.querySelector(".gift-content");
+
+function init(){
+  gift.addEventListener("click", openGift);
+  gift.classList.add("dukdik")
+}
 
 function openGift(){
   giftTop.classList.add("boxOpen");
   giftBox.classList.add("boxDown");
-  body.classList.add("open")
-
+  body.classList.add("open");
+  gift.classList.remove("dukdik");
+  giftContent.classList.add("content-appearance")
 }
+
+init();
